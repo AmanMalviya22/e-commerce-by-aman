a4f938a28b1ef249dda8023e73c570ef333d434f
Aman Malviya <amanaman26802@gmail.com>
2023-09-16 13:47:26 +0530

    updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // connecting mongoose to its default server and ecommerceDB
-mongoose.connect('mongodb://localhost:27017', {
+mongoose.connect('mongodb://localhost:27017/ecommerceDB', {
     useNewUrlParser: true
 });
 
